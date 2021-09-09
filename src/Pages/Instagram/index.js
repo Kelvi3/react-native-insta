9aea23fa5fd5707667a3bfaae7e606d505df1364
Kelvi3 <theocazenavecoupet@gmail.com>
2021-09-09 11:17:45 +0200

    ADD: pages home

---
+++ src/Pages/Instagram/index.js
@@ -0,0 +1,14 @@
+import React from "react" 
+import { Text, View } from "react-native"
+
+const Instagram = () => {
+   
+  return(
+    <View>
+      <Text>BAW</Text>
+      <Text>BAW</Text>
+    </View>
+  )
+}
+
+export default Instagram
