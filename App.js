5986c9136cbc0d37c78be287e6eb607271df79be
Kelvi3 <theocazenavecoupet@gmail.com>
2021-09-09 14:09:19 +0200

    UPDATE: component

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import SignUp from './src/Pages/SignUp';
-import SignIn from './src/Pages/SignIn';
-import Instagram from './src/Pages/Instagram';
+import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Cookies from 'js-cookie';
-import { Provider, useSelector } from 'react-redux';
+import { Provider} from 'react-redux';
 import store from './src/redux/store';
+import BottomTab from './src/Component/BottomTab';
+import LogedNavigation from './src/Component/LogedNavigation';
 
-const Stack = createNativeStackNavigator();
-
-// screenOptions={{ headerShown: false }}
 
 const App = () => {
 
   return (
     <Provider store={store}>
       <NavigationContainer>
-          <Stack.Navigator> 
-            <Stack.Screen name="SignIn" component={SignIn} />  
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="Instagram" component={Instagram} /> 
-          </Stack.Navigator>
+        <LogedNavigation />
+        <BottomTab/>
       </NavigationContainer>
     </Provider>
   );
