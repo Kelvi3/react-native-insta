d2aae51594dec98c237b65dcb5aa60153d82d5c5
Kelvi3 <theocazenavecoupet@gmail.com>
2021-09-09 11:17:02 +0200

    ADD: redux provider and pages home

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import SignUp from './src/Pages/SignUp';
 import SignIn from './src/Pages/SignIn';
-import Home from './src/Pages/Home';
+import Instagram from './src/Pages/Instagram';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Cookies from 'js-cookie';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import store from './src/redux/store';
 
 const Stack = createNativeStackNavigator();
 
+// screenOptions={{ headerShown: false }}
 
 const App = () => {
+
   return (
     <Provider store={store}>
       <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}> 
+          <Stack.Navigator> 
             <Stack.Screen name="SignIn" component={SignIn} />  
             <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="Home"   component={Home} /> 
+            <Stack.Screen name="Instagram" component={Instagram} /> 
           </Stack.Navigator>
       </NavigationContainer>
     </Provider>
