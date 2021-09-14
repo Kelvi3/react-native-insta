fd3a0f2fa978d92ef98b17c59ddaab0323047058
Kelvi3 <theocazenavecoupet@gmail.com>
2021-09-14 12:23:02 +0200

    ADD: take photo

--- src/Pages/Picture/index.js
+++ src/Pages/Picture/index.js
@@ -1,10 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import Photo from '../../Component/Photo';
+// import Photo from '../../Component/Photo';
+import Publish from '../../Component/Publish';
+import { Camera } from 'expo-camera';
 
 export default function Picture() {
   const [selectedImage, setSelectedImage] = React.useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [image, setImage] = React.useState(null)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+  
+  const _takePhoto = async () => {
+    const photo = await ref.current.takePictureAsync()
+    setImage(photo.uri)
+  }
 
   let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -22,20 +40,59 @@ export default function Picture() {
     setSelectedImage({ localUri: pickerResult.uri });
   };
 
-  if (selectedImage !== null) {
+  if (image !== null || selectedImage !== null) {
     return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
-        />
+      <>
+      <View>
+        {image === null ? 
+          <Publish image={selectedImage.localUri} btn={() => setSelectedImage(null)}/>
+        :
+          <Publish image={image} btn={() => setImage(null)}/>
+        }
       </View>
+      </>
     );
   }
 
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <>
-    <Photo/>
+        <View style={{ flex: 1 }}>
+      <Camera style={{ height: 550 }} type={type} ref={ref}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <TouchableOpacity onPress={_takePhoto}>
+        <Image source={require("./button.svg.png")}
+        style={{width: 60, height: 60, alignSelf: "center", marginTop: 10 }} />
+      </TouchableOpacity>
+    </View>
+    {/* <Photo/> */}
     <View style={styles.container}>
       <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
       <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
