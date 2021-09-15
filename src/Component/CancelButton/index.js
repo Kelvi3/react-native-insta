5df4a51bb3c16a78ebe0594e097beed68e08bcbf
Kelvi3 <theocazenavecoupet@gmail.com>
2021-09-15 15:45:49 +0200

    ADD: cancel button return back

---
+++ src/Component/CancelButton/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Text} from "react-native"
+
+const CancelButton = () => {
+  return(
+    <Text style={{fontSize: 25}}>←</Text>
+  )
+}
+
+
+export default CancelButton
