b17dfeafb6aadb40cb58c3ec1fedfb8d804e64f8
Kelvi3 <theocazenavecoupet@gmail.com>
2021-09-15 15:46:37 +0200

    ADD: arrow return back

--- src/Component/Publish/index.js
+++ src/Component/Publish/index.js
@@ -1,25 +1,39 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import { Image, View, TouchableOpacity, Text, TextInput } from "react-native"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Publish = (props) => {
+const Publish = () => {
+  const [pic, setPic] = useState("")
+  
+  useEffect(() => {
+    getPic()
+  }, [])
+  
+  const getPic = async () => {
+    try {
+      const value = await AsyncStorage.getItem("pic")
+      setPic(value)
+      return value
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  
 
-  return(
-    <View style={{paddingTop: 20}}>
-      <View style={{alignItems: 'center'}}>
-        <Image style={{width: 400, height: 300, resizeMode: "contain"}} source={{ uri: props.image }}/>
-        <TextInput placeholder="Description" style={{width: 300, height: 30, borderColor: "#808080", borderWidth: 1, backgroundColor: "#E4E4E4" }} />
-        <TouchableOpacity style={{backgroundColor: "green", padding: 5, width: 120}}>
-          <Text style={{color: "white", textAlign: "center"}}>Poster</Text>
-        </TouchableOpacity>
+
+  if (pic !== ""){ 
+    console.log(pic)
+    // , resizeMode: "contain", marginBottom: 5
+    return(
+      <View style={{paddingTop: 20}}>
+        <View style={{alignItems: 'center'}}>
+          <Image style={{width: 400, height: 300}} source={{uri: pic }}/>
+          <TextInput placeholder="Description" style={{padding: 5, marginBottom: 5, width: 300, height: 30, borderColor: "#808080", borderWidth: 1, backgroundColor: "#E4E4E4" }} />
+        </View>
       </View>
-      <View> 
-        <TouchableOpacity onPress={props.btn} style={{backgroundColor: "red", padding: 5, width: 120, alignItems: "flex-end"}}>
-          <Text style={{color: "white"}}>Recommencer la photo</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  )
+    )
+  }else return <View/>
 }
 
 export default Publish
